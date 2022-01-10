0cc0c3e55882f393f108261d5a001776fbd6c8f5
rohitwtbs <12rohit4@gmail.com>
2022-01-11 01:34:04 +0530

    day 1 of making portfolio in dash

---
+++ app.py
@@ -0,0 +1,51 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+            "Amount": [4, 1, 2, 2, 4, 5],
+                "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+                })
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+        html.H1(children='Hello i am rohit'),
+
+            html.Div(children='''
+                    Worked as backend,frontend,fullstack,hybrid app developer.
+                    And now getting my hands dirty in extracting oil i.e
+                    information from Data
+                        '''),
+            html.H1(children='Languages'),
+            html.H3(children='Python'),
+            html.H3(children='Node.js'),
+            html.H3(children='Java'),
+            html.Label('Python'),
+            dcc.Slider(
+                min=0,
+                max=9,
+                marks={i:'Label {}'.format(i)if i==1 else str(i) for i in range(1,6)},
+                value=3
+
+                )
+
+#                dcc.Graph(
+#                            id='example-graph',
+#                                    figure=fig
+#                                        )
+               ])
+
+if __name__ == '__main__':
+        app.run_server(debug=True)
+
