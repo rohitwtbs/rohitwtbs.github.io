c73d7436e469648ba984c2a46cfd060a4b2b5250
rohitwtbs <12rohit4@gmail.com>
2022-01-25 00:38:50 +0530

    using a library to make a grid and i just dump elements there

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 import dash
+import dash_flexbox_grid as dfx
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
@@ -23,7 +24,7 @@ df = pd.DataFrame({
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div(children=[
+left_side = html.Div(children=[
         html.H1(children='Hello i am rohit'),
 
             html.Div(children='''
@@ -61,14 +62,42 @@ app.layout = html.Div(children=[
                 ],
                 style={'width':'100%','height':'50vh','margin':"auto","display":"block","position":"relative"}
                 )
-
-#                dcc.Graph(
-#                            id='example-graph',
-#                                    figure=fig
-#                                        )
                ],
                style={'background':'wheat','width':'30%'}
                )
+
+
+# let me try two div's in parallel
+
+app.layout = html.Div(children=[
+        html.Div(children='Hello i am rohit',style={}),
+        html.Div(children='Hello i am rohit',style={}),
+
+])
+
+
+app.layout = dfx.Grid(id='grid', fluid=True, children=[
+    dfx.Row(children=[
+        dfx.Col( children=[
+            left_side
+        ]),
+        dfx.Col( children=html.Div('Hello, World!'))
+    ]),
+    dfx.Row(id='row', children=[
+        dfx.Col(id='col', xs=6, lg=2, children=html.Div('Hello, World!')),
+        dfx.Col(xs=6, lg=2, children=html.Div('Hello, World1!')),
+        dfx.Col(xs=6, lg=2, children=html.Div('Hello, World2!')),
+        dfx.Col(xs=6, lg=2, children=html.Div('Hello, World!3')),
+        dfx.Col(xs=6, lg=2, children=html.Div('Hello, World!4')),
+        dfx.Col(xs=6, lg=2, children=html.Div('Hello, World!4')),
+        dfx.Col(xs=6, lg=2, children=html.Div('Hello, World!4')),
+        dfx.Col(xs=6, lg=2, children=html.Div('Hello, World!4')),
+        dfx.Col(xs=6, lg=2, children=html.Div('Hello, World!4')),
+        dfx.Col(xs=6, lg=2, children=html.Div('Hello, World!4')),
+        dfx.Col(xs=6, lg=2, children=html.Div('Hello, World!4'))
+    ])
+])
+    
 
 if __name__ == '__main__':
         app.run_server(debug=True)
